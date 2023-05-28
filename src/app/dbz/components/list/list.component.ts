fcc0dd047be2615ad15425d8ef10eb9d97d80223
Hugo Vicente Peligro <jchugo55@gmail.com>
2023-05-28 23:29:44 +0200

    subida 02

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+  @Input()
+  public characterList : Character[] = [{'name': "Trunks", "power": 10}];
+
+  @Output()
+  public onDeleteIndex: EventEmitter<string> = new EventEmitter();
+
+onDelete(id?: string){
+  if(!id) return;
+  this.onDeleteIndex.emit(id);
+
+}
+
+  onDeleteCharacter(indice: number):void{
+    //TODO: Emitir id
+  }
+}
